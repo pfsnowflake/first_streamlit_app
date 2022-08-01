0c250a84c56c8fec9c02b1a7310a809775a225c6
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-01 14:43:13 -0500

    Update streamlit_app.py
    Fix type, added breakfeast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-Import streamlit
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Breakfest Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie ')
+streamlit.title('Hard-Boiled Free-Range Egg')
