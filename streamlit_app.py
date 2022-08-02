efb2b2b397d475939020f181ae284ad7635171fa
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-02 08:57:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
-streamlit.dataframe(my_fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 
