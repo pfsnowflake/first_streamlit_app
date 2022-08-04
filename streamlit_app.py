210c8c652ef3e0c42c7369080df234f3ba12006d
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-04 07:50:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi"
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Take json verson of response and Normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Format response
 streamlit.dataframe(fruityvice_normalized)
-
-#27 streamlit.write('The user entered ', fruit_choice)
-
-#
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
-#streamlit.text(fruityvice_response.json())  # just writes header to screen
