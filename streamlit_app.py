3e3220fe97ed5d04fb98dfe630c09861fa8b7088
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-04 15:37:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to disply fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+  IF nor fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+  #import request
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # Take json verson of response and Normalize it
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # Format response
+     streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Take json verson of response and Normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Format response
-streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot 
 streamlit.stop()
