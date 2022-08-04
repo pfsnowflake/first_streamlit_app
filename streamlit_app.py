cca95efee34f107cb41e152bb88a36e4428b45cc
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-04 15:43:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,14 +29,12 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   IF not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
+    streamlit.eror("Please select a fruit to get information.")
   else:
-  #import request
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  # Take json verson of response and Normalize it
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # Format response
      streamlit.dataframe(fruityvice_normalized)
+      
 except URLError as e:
   streamlit.error()
   
