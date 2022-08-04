0cc72593362817d7191926aa28dc9dbd3670bdca
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-04 15:14:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,6 @@ streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to a list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered', add_my_fruit)
+
