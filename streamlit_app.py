a47d7a3f03ac3ccd11e941fa16733d4c32718edd
pfsnowflake <110422525+pfsnowflake@users.noreply.github.com>
2022-08-04 07:51:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "apple")
 
 # Take json verson of response and Normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
